8323ddee326ba603f486f9dc84e66f72a3fb31aa
gracexingluv <gracexingluv@gmail.com>
2020-12-05 23:51:34 +1100

    upload

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,7 @@
+// from samples.json
+// var tableData = samples;s
+var tableData = require('02-Homework\15-Interactive-Visualizations-and-Dashboards\Instructions\StarterCode\samples.json');
+
+console.log(tableData)
+
+// YOUR CODE HERE!
