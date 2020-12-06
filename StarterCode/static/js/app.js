5c8284489c02feecc1cf9f1c71e2838ece0894a1
gracexingluv <gracexingluv@gmail.com>
2020-12-06 11:08:02 +1100

    update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,7 +1,3 @@
-// from samples.json
-// var tableData = samples;s
-var tableData = require('02-Homework\15-Interactive-Visualizations-and-Dashboards\Instructions\StarterCode\samples.json');
-
-console.log(tableData)
-
-// YOUR CODE HERE!
+d3.json("../../samples.json").then((samples) => {
+    console.log(samples.names);
+})
